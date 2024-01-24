48fe602164cd37bf112e9d1e12fa571b414ee237
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-24 16:37:36 +0800

    Update AnnoBuildTest0.java

--- AnnoBuildTest0.java
+++ AnnoBuildTest0.java
@@ -1,6 +1,8 @@
-import org.hibernate.validator.internal.util.annotationfactory.AnnotationProxy;
+//import org.hibernate.validator.internal.util.annotationfactory.AnnotationProxy;
+import org.hibernate.validator.cfg.context.TypeConstraintMappingContext;
 public class funcsContent {
 
-@org.hibernate.validator.internal.util.annotationfactory.AnnotationProxy()
+//@org.hibernate.validator.internal.util.annotationfactory.AnnotationProxy()
+@org.hibernate.validator.cfg.context.TypeConstraintMappingContext()
 private String s;
 }
